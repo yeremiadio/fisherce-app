3c0058af0fd5163a280aa034527fd9df6d6addf8
Yeremia Alfa Dio <raikkonendio@gmail.com>
2022-03-26 01:38:59 +0700

    env added

--- components/Screens/HomeScreen.js
+++ components/Screens/HomeScreen.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image } from "react-native";
+import { IMAGE_URL } from "@env";
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
+      <Image
+        source={{ uri: `${IMAGE_URL}` }}
+        style={{ width: 200, height: 200 }}
+      />
       <Button title="Login" onPress={() => navigation.navigate("Login")} />
       <Button
         title="Register"
